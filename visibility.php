d6379f3eb539f3a9cb439783066bf31ec7a15e75
Dede Yusup Somali <onlinezimix@gmail.com>
2019-10-04 11:38:46 +0700

    Menambahkan visibility.php dan perbaikan overriding.php

--- visibility.php
+++ visibility.php
@@ -8,11 +8,13 @@ class produk {
     // property
     public $judul ,
            $penulis ,
-           $penerbit ,
-           $harga ;
+           $penerbit;
+// visibility         
+  protected $diskon = 0 ;
+    private $harga ;
         
   // constructor
-         public function __construct ($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0){
+  public function __construct ($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0){
           // memanggil property
           $this->judul = $judul;
           $this->penulis = $penulis;
@@ -20,14 +22,17 @@ class produk {
           $this->harga = $harga;
     
          }
+  public function getHarga(){
+    return $this->harga - ($this->harga * $this->diskon / 100);
+  }
     // method
-           public function getlabel(){
-            return "$this->penulis, $this->penerbit";
+    public function getlabel(){
+      return "$this->penulis, $this->penerbit";
            }
-         public function getInfoProduk(){
-          $str = "{$this->judul} | {$this->getlabel()} (Rp. {$this->harga})";
+  public function getInfoProduk(){
+    $str = "{$this->judul} | {$this->getlabel()} (Rp. {$this->harga})";
           
-          return $str;
+    return $str;
          }
 }
 // class inheritance
@@ -49,7 +54,10 @@ class game extends produk{
     
     $this->waktuMain = $waktuMain;
   }
-public function getInfoProduk (){
+  public function setDiskon($diskon) {
+    $this->diskon = $diskon;
+  }
+  public function getInfoProduk (){
       $str = "game : ". parent::getInfoProduk() . " - {$this->waktuMain} jam.";
       return $str;
   }
@@ -61,10 +69,14 @@ class CetakInfoProduk {
   }
 }
    // instansiasi
-    $produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100, 0); 
+   $produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100, 0); 
 
     $produk2 = new Game("Uncharted", "Neil Druckman", "Sony Computer", 250000, 0, 50); 
   echo $produk1->getInfoProduk();
   echo "<br>";
   echo $produk2->getInfoProduk();
+  echo "<hr>";
+  
+  $produk2->setDiskon(50);
+  echo $produk2->getHarga();
  ?>
