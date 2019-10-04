1e234398682c8d635c8ec58a14ad96966a155c4e
Dede Yusup Somali <onlinezimix@gmail.com>
2019-10-04 12:54:45 +0700

    Memperbaiki init.php di folder namespace/Services

--- namespace/App/init.php
+++ namespace/App/init.php
@@ -4,6 +4,17 @@
 // require_once 'Produk/komik.php';
 // require_once 'Produk/Game.php';
 // require_once 'Produk/CetakInfoProduk.php';
+// require_once 'Produk/User.php';
+// require_once 'Service/User.php';
 spl_autoload_register(function($class){
+	// App\Produk\User = ["App","Produk","User"]
+	$class = explode('\\', $class);
+	$class = end($class);
 	require_once __DIR__ . '/Produk/' .$class.'.php';
+});
+spl_autoload_register(function($class){
+// 	// App\Service\User = ["App","Service","User"]
+    $class = explode('\\', $class);
+	$class = end($class);
+	require_once __DIR__ . '/Service/' .$class.'.php';
 });
