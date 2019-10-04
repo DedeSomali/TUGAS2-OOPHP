a30d0084ac9d08f6286c9d458e2cdcbe5e05ebb2
Dede Yusup Somali <onlinezimix@gmail.com>
2019-10-04 12:27:41 +0700

    Memperbaiki index.php folder namespace

--- namespace/index.php
+++ namespace/index.php
@@ -1,14 +1,18 @@
+
 <?php 
-require_once 'App/init.php';
-	$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100, 0); 
-
- 	$produk2 = new Game("Uncharted", "Neil Druckman", "Sony Computer", 250000, 0, 50); 
-
-
-  $cetakProduk = new CetakInfoProduk();
-  $cetakProduk->tambahProduk($produk1);
-  $cetakProduk->tambahProduk($produk2);
-  echo $cetakProduk->cetak();
+ require_once 'App/init.php';
+//  $produk1 = new komik("naruto", "massashi kishimoto", "shonen jump" ,30000 , 100);
+//   $produk2 = new game("uncharted", "neil drackman", "sony computer", 25000 , 50);
  
-  echo"<hr>";
-  new Coba();
+//   $cetakProduk = new CetakInfoProduk();
+//   $cetakProduk->tambahProduk($produk1);
+//   $cetakProduk->tambahProduk($produk2);
+//   echo $cetakProduk->cetak();
+ 
+//   echo"<hr>";
+  use App\Service\User as ServiceUser;
+  use App\Produk\User as ProdukUser;
+  
+  new ServiceUser();
+  echo "<br>";
+  new ProdukUser();
